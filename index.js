b115c8696d990de0e56fdbf0a7e2ef63986154c0
baw <52800161+vikramatter@users.noreply.github.com>
2021-09-17 06:41:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+var readlineSync = require('readline-sync');
+
+var score = 0;
+
+// data of high score
+var highScores = [
+	{
+		name: 'Bawa',
+		score: 10
+	},
+
+	{
+		name: 'Vikram',
+		score: 9
+	}
+];
+
+// array of objects
+var questions = [
+	{
+		question: 'Which team is the champions of Europe? ',
+		answer: 'Chelsea'
+	},
+	{
+		question: 'Who Scored the Goal In Ucl final? ',
+		answer: 'Kai Havertz'
+	},
+	{
+		question: 'How MAny Ucl titles Have Chelsea Won? ',
+		answer: 'Two'
+	},
+  {
+  question:'Which team did Chelsea played against in Super Cup Final?',
+  answer:'Villareal'},
+  {question:'Who is the manager of Chelsea?',
+  answer:'Tuchel',
+  },
+  {
+    question:'Who was the predecessor Of Tuchel?',
+    answer:'Lampard'
+  },
+  {
+    question:'Was Lampard a Chelsea Player?',
+    answer:'Yes'
+  },
+  {
+    question:'In which Position lampard played at chelsea?',
+    answer:'Midfield'
+  },
+  {
+     question:'Which club did tuchel left to join chelsea?',
+    answer:'Psg'
+  }
+];
+
+function benvenuto() {
+	var userName = readlineSync.question("Buongiorno! What's your name? ");
+
+	console.log('Benventuro! ' + userName + ' to Football Quiz');
+}
+
+// play function
+function play(question, answer) {
+	var userAnswer = readlineSync.question(question);
+
+	if (userAnswer.toLowerCase() === answer.toLowerCase()) {
+		// branching
+		console.log('Siuuu Bravo !');
+		score = score +3 ;
+	} else {
+		console.log('Oh! Oh! La prossima volta sarai più fortunato ! -1 for wrong answers');
+    score=score-1;
+	}
+
+	console.log('current score: ', score);
+	console.log('-------------');
+}
+
+function game() {
+	for (var i = 0; i < questions.length; i++) {
+		var currentQuestion = questions[i];
+		play(currentQuestion.question, currentQuestion.answer);
+	}
+}
+
+function showScores() {
+	console.log('Grazie! You SCORED: ', score);
+
+	console.log(
+		"Check out the high scores, if you should be there ping me and I'll update it"
+	);
+  console.log("\nThese are the top scorers")
+console.log("Name\t\tScore")
+for(var i = 0; i < highScores.length; i++) {
+  console.log(highScores[i].name + "\t\t" + highScores[i].score);
+}
+
+}
+
+
+benvenuto();
+game();
+showScores();
